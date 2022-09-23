28c9a9ce373a9c2565c9a6e106f9a5965df6135e
ImValerio <valerio.valletta@icloud.com>
2022-09-23 15:00:27 +0200

    working recipe service + selected recipe

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  styleUrls: ['./recipe.component.css'],
+  providers: [RecipeService]
 })
 export class RecipeComponent implements OnInit {
-  constructor() { }
+  selectedRecipe: Recipe;
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      console.log(recipe)
+      this.selectedRecipe = recipe
+    })
   }
 
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService {
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Test recipe', 'this is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ]
+
+  getRecipes() {
+    return this.recipes.slice(); //return a copy of the array
+  }
 }
