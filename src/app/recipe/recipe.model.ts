fba58586afeac2417821ed94cc04e95ebea8a9dc
ImValerio <valerio.valletta@icloud.com>
2022-09-23 15:39:10 +0200

    add 'add to shopping list' functionality

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
 
   public name: String;
   public description: String;
   public imagePath: String;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, img: string) {
+  constructor(name: string, desc: string, img: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = img;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,13 +1,30 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Test recipe', 'this is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+    new Recipe('Burger',
+      'this is a burger',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+
+      ])
+
   ]
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice(); //return a copy of the array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
